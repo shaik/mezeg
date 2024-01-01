3d6e7579409c4a83ae243c2d43b9871788dbb249
Shai Kfir <shaik@wix.com>
2024-01-01 21:54:55 +0200

    added multiple days

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, jsonify
-from datetime import datetime
+from datetime import datetime, timedelta
+
 import pandas as pd
 import requests
 
-api_key = 'X'
+api_key = ''
 
 app = Flask(__name__)
 cities_df = pd.read_csv('data/worldcities.csv')
+NUM_YEARS = 3
+NUM_DAYS = 3
 
 
 def K2C(kelvin_temp, decimal_places=2):
@@ -29,34 +32,34 @@ def cities():
 @app.route('/get_weather', methods=['POST'])
 def get_weather():
     city = request.form.get('city')
-    date = request.form.get('date')
+    selected_date = datetime.strptime(request.form.get('date'), '%Y-%m-%d')
 
-    # Find the coordinates of the city
     city_data = cities_df[cities_df['city'] == city].iloc[0]
     lat = city_data['lat']
     lon = city_data['lng']
 
     weather_data = []
-    for i in range(1, 4):  # Last 3 years
-        year = int(date.split('-')[0]) - i
-        new_date = f"{year}-{date.split('-')[1]}-{date.split('-')[2]}"
-        unix_time = int(datetime.strptime(new_date, '%Y-%m-%d').timestamp())
-        response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={unix_time}&appid={api_key}")
-        if response.status_code == 200:
-            year_data = response.json()
-            year_data['year'] = year  # Add the year to the data
-            for data in year_data['data']:
-                # Convert Unix timestamp to a formatted date string
-                data['formatted_date'] = datetime.utcfromtimestamp(data['dt']).strftime('%Y-%m-%d')
-                if 'temp' in data:
-                    data['temp'] = K2C(data['temp'])
-                if 'feels_like' in data:
-                    data['feels_like'] = K2C(data['feels_like'])
-            weather_data.append(year_data)
-        else:
-            weather_data.append({'error': 'No data', 'year': year})
+    for i in range(NUM_YEARS):
+        year = selected_date.year - i
+        yearly_data = []
+        for j in range(NUM_DAYS):
+            date_to_fetch = datetime(year, selected_date.month, selected_date.day) + timedelta(days=j)
+            unix_time = int(date_to_fetch.timestamp())
+            response = requests.get(f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={unix_time}&appid={api_key}")
+            if response.status_code == 200:
+                daily_data = response.json()['data'][0]
+                daily_data['formatted_date'] = date_to_fetch.strftime('%Y-%m-%d')
+                if 'temp' in daily_data:
+                    daily_data['temp'] = K2C(daily_data['temp'])
+                if 'feels_like' in daily_data:
+                    daily_data['feels_like'] = K2C(daily_data['feels_like'])
+                yearly_data.append(daily_data)
+            else:
+                yearly_data.append({'error': 'No data', 'date': date_to_fetch.strftime('%Y-%m-%d')})
+        weather_data.append({'year': year, 'data': yearly_data})
 
-    return render_template('weather_results.html', weather_data=weather_data, city=city, date=date)
+    return render_template('weather_results.html', weather_data=weather_data, city=city)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
